229577cb6da904ad21165eeb67180c605df04eb1
Qharny <cannycc1020@gmail.com>
2024-05-01 15:19:52 +0000

    modified

--- lib/constant/color.dart
+++ lib/constant/color.dart
@@ -4,3 +4,4 @@ import 'package:flutter/material.dart';
 const bgcolor = Color(0xFFFFD69A);
 const whitecolor = Color(0xFFFFFFFF);
 const primaryColor = Color(0xfffaaf4b);
+const textColor = Color(0xff212e47);
--- lib/screen/role_select.dart
+++ lib/screen/role_select.dart
@@ -1,35 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:role_select/constant/color.dart';
 
-class roleselect extends StatefulWidget {
-  const roleselect({super.key});
+class RoleSelect extends StatefulWidget {
+  const RoleSelect({Key? key}) : super(key: key);
 
   @override
-  State<roleselect> createState() => _roleselectState();
+  State<RoleSelect> createState() => _RoleSelectState();
 }
 
-class _roleselectState extends State<roleselect> {
+class _RoleSelectState extends State<RoleSelect> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        backgroundColor: bgcolor,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // Image(
-              //   image: AssetImage('assets/BOffice.jpg'),
-              //   fit: BoxFit.cover,
-              // ),
-              Text(
-                'Select your role',
-                style: TextStyle(
-                    color: primaryColor,
-                    fontSize: 35.0,
-                    fontWeight: FontWeight.w700),
-              ),
-            ],
-          ),
-        ));
+    return Scaffold(
+      backgroundColor: bgcolor,
+      appBar: AppBar(
+        toolbarHeight: 410,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        flexibleSpace: const Image(
+          image: AssetImage('assets/image/BOffice.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text(
+              'Select your role',
+              style: TextStyle(
+                  color: primaryColor,
+                  fontSize: 35.0,
+                  fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            _button('Hostel Manager'),
+            const SizedBox(
+              height: 20.0,
+            ),
+            _button('Agent'),
+            const SizedBox(
+              height: 20.0,
+            ),
+            _button('Guest House'),
+            const SizedBox(
+              height: 20.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _button(String buttonText) {
+    return Container(
+      width: 300,
+      height: 80,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: whitecolor,
+      ),
+      child: ElevatedButton(
+        onPressed: () {
+          // Add your button's onPressed function here
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: whitecolor,
+          elevation: 0.0,
+        ),
+        child: Text(
+          buttonText,
+          style: const TextStyle(
+              fontSize: 28, color: textColor, fontWeight: FontWeight.w700),
+        ),
+      ),
+    );
   }
 }
